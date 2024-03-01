6b379fcda159d02e50d2d4559621ec56f1535564
aarslantas <a_arslantas69@hotmail.com>
2024-03-01 23:45:40 +0300

    complete signup connection

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,16 +1,19 @@
 import { createClient } from "@supabase/supabase-js";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 // export const supabase = createClient(
 //   process.env.NEXT_PUBLIC_SUPABASE_URL!,
 //   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 // );
 
-export const supabase = createClient(
-  "https://qjdeieadjiyilijbqqrm.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqZGVpZWFkaml5aWxpamJxcXJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc3NjI5MzEsImV4cCI6MjAyMzMzODkzMX0.7-egYKLrWb7qJ8fLOauwfRCW1yXUqZ1jMjBk5H-LRuc"
-);
+// export const supabase = createClient(
+//   "https://qjdeieadjiyilijbqqrm.supabase.co",
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqZGVpZWFkaml5aWxpamJxcXJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc3NjI5MzEsImV4cCI6MjAyMzMzODkzMX0.7-egYKLrWb7qJ8fLOauwfRCW1yXUqZ1jMjBk5H-LRuc"
+// );
 
-export const supabaseAdmin = createClient(
-  "https://qjdeieadjiyilijbqqrm.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqZGVpZWFkaml5aWxpamJxcXJtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNzc2MjkzMSwiZXhwIjoyMDIzMzM4OTMxfQ.QX-yhWnNMuIVqrvRncaxwl2BgIdNWIITsJQIvU7JCJ4"!
-);
+// export const supabaseAdmin = createClient(
+//   "https://qjdeieadjiyilijbqqrm.supabase.co",
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqZGVpZWFkaml5aWxpamJxcXJtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNzc2MjkzMSwiZXhwIjoyMDIzMzM4OTMxfQ.QX-yhWnNMuIVqrvRncaxwl2BgIdNWIITsJQIvU7JCJ4"!
+// );
+
+export const supabase = createClientComponentClient();
